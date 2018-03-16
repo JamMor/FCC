f5e25a3914019734c2a4e3714f32d057ab402d55
JamMor <JamMor@gmail.com>
2018-03-16 05:29:49 -0500

    Update 012 - Mutations.js

--- 012 - Mutations.js
+++ 012 - Mutations.js
@@ -1,1 +1,29 @@
 
+function mutation(arr) {
+  var uniqueLetters = {};
+  var letters = arr[0].toLowerCase().split('');
+  letters.forEach(function(element) {
+    uniqueLetters[element] = true;
+  })
+  var compareLetters = arr[1].toLowerCase().split('');
+  for (i = 0; i < compareLetters.length; i++) {
+    if (uniqueLetters.hasOwnProperty(compareLetters[i]) === false) {
+      return false;
+    }
+  }
+  return true;
+}
+
+
+//======================= A version using indexOf =======================
+
+function mutation2(arr) {
+  var basketString = arr[0].toLowerCase();
+  var letters = arr[1].toLowerCase().split('');
+  for (var i = 0; i < letters.length; i++) {
+    if (basketString.indexOf(letters[i]) < 0) {
+      return false;
+    }
+  }
+return true;
+}
